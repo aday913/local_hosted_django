d4e174347d11f7c197995d682d0dbeb3af276058
Alex Day <alexdaygjj@gmail.com>
2022-08-22 01:33:58 -0700

    Add plant_monitoring app urls to main

--- lan_project/lan_project/urls.py
+++ lan_project/lan_project/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('plant_monitoring/', include(('plant_monitoring.urls', 'plant_monitoring'), namespace='plant_monitoring'))
 ]
