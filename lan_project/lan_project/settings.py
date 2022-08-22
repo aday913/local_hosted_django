d6de14029e1fe63eaddfc48892bce08b0ba0e07c
Alex Day <alexdaygjj@gmail.com>
2022-08-22 01:18:08 -0700

    Add path to templates

--- lan_project/lan_project/settings.py
+++ lan_project/lan_project/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'lan_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
